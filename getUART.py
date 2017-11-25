77782ea4071ec762191af06d825331c032d71290
julien LM <julien.lemellec@gmail.com>
2017-11-25 16:23:17 +0100

    [Python]: define main class

--- getUART.py
+++ getUART.py
@@ -1,1 +1,16 @@
-print "Hello World!!"
+#!/usr/bin/env python
+
+import sys
+
+
+class Main:
+    def __init__(self):
+        print sys.argv
+
+    def main(self, argv):
+        print argv[1]
+
+
+if __name__ == "__main__":
+    Main().main(sys.argv)
+
