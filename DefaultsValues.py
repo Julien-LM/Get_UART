ee4d2d08a6dfeba885f07e9027b0e45606c21efb
Julien-LM <julien.lemellec@gmail.com>
2017-12-08 00:18:56 +0100

    [python]: start user interface
    	TODO:
    	- Enum choices
    	- Every function regarding choices
    	- Time fct?
    	- UART communication with PIC
    	- UART protocole with PIC

--- DefaultsValues.py
+++ DefaultsValues.py
@@ -9,3 +9,36 @@
 """
 
 COM_PORT = '/dev/ttyUSB0'
+
+# ASCII definition
+END_OF_TRANSMIT         = 0x04
+START_OF_TEXT           = 0x02
+ACKNOWLEDGE             = 0x06
+LINE_FEED               = 0x0A
+CARRIAGE_RETURN         = 0x0D
+NEG_ACKNOWLEDGE         = 0x15
+BACKSPACE               = 0x08
+
+# Errors codes
+WRONG_ARGUMENTS         = 0x41
+UNKNOWN_COMMAND         = 0x42
+BUFFER_OVERFLOW         = 0x43
+DEVICE_BUSY             = 0x44
+FRAMING_ERROR           = 0x45
+OVERRUN_ERROR           = 0x46
+
+# Communication Protocol
+GET_TEMP                = 0x20
+GET_TIME                = 0x21
+SET_TIME                = 0x22
+CONFIGURE_SENSOR        = 0x30
+CLEAN_DATA              = 0x31
+GET_DATA_NUMBER         = 0x32
+
+# Commands args size
+GET_TEMP_SIZE           = 0x00
+GET_TIME_SIZE           = 0x00
+SET_TIME_SIZE           = 0x06
+CONFIGURE_SENSOR_SIZE   = 0x02
+CLEAN_DATA_SIZE         = 0x00
+GET_DATA_NUMBER_SIZE    = 0x00
--- getUART.py
+++ getUART.py
@@ -10,6 +10,7 @@
 """
 
 import sys
+import os
 
 from Init import Init
 from UART import UART
@@ -32,9 +33,32 @@ class Main:
         """
         Main script function
         """
-        self.logger.info("Let's try UART!!!")
-        serial_com = UART(logger=self.logger, port=self.init.get_serial_port())
-        serial_com.playUART()
+        os.system('clear')
+        self.logger.info("#################################################################")
+        self.logger.info("                    Welcome to PIC interface                     ")
+        self.logger.info("#################################################################")
+
+        self.logger.info("\nSelect one of the following action:")
+        self.logger.info("1) Get temp")
+        self.logger.info("2) Get time")
+        self.logger.info("3) Set time")
+        self.logger.info("4) Configure sensor")
+        self.logger.info("5) Clean data")
+        self.logger.info("6) Get data number")
+
+        self.logger.info("7) Export data current file")
+        self.logger.info("8) Export data new file")
+
+        while 1:
+            # get keyboard input
+            input = raw_input(">> ")
+            print input
+            if input == 'exit':
+                exit()
+
+
+        # serial_com = UART(logger=self.logger, port=self.init.get_serial_port())
+        # serial_com.playUART()
 
 
 if __name__ == "__main__":
